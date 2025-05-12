2c47f83c9a62b706f3240ff3012a221b822ecafd
Si Pavitt <s.pavitt@live.co.uk>
2025-05-12 17:16:58 +0000

    Completed participant API endpoints with group balancing and withdrawal

--- server/routes/participantRoutes.js
+++ server/routes/participantRoutes.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { createParticipant } = require('../controllers/participantController');
+const {
+  createParticipant,
+  saveConsent,
+  saveDemographics,
+  assignGroup,
+  markInterventionComplete,
+  savePostSurvey,
+  withdrawParticipant
+} = require('../controllers/participantController');
 
 router.post('/start', createParticipant);
+router.post('/consent', saveConsent);
+router.post('/demographics', saveDemographics);
+router.post('/assign-group', assignGroup);
+router.post('/complete-intervention', markInterventionComplete);
+router.post('/post-survey', savePostSurvey);
+router.post('/withdraw', withdrawParticipant);
 
 module.exports = router;
